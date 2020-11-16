973c1f0c703fdcd187574fb7e618d40966ef42db
jotabraham <jotabraham@gmail.com>
2020-11-16 10:36:28 -0500

    completed tutorial with ERRORS

--- main.js
+++ main.js
@@ -16,8 +16,7 @@ function play(e) {
     const computerChoice = getComputerChoice();
     const winner = getWinner(playerChoice, computerChoice);
     showWinner(winner, computerChoice);
-    console.log(playerChoice, computerChoice, winner);
-};
+}
 
 //computer choice
 function getComputerChoice() {
@@ -57,28 +56,28 @@ function getWinner(p, c) {
 };
 
 //show winner
-function showWinner (winner, computerChoice) {
+function showWinner(winner, computerChoice) {
     if (winner === 'player') {
-        //inc player score by 1
+        // inc player score by 1
         scoreboard.player++;
-        //show modal result
+        // show modal result
         result.innerHTML = `
-        <h1 class="text-win">You DID u son uv uh B!TCH</h1>
+        <h1 class="text-win">You WIN!</h1>
         <i class="fas fa-hand-${computerChoice} fa-10x"></i>
         <p>Computer chose <strong>${computerChoice}</strong></p>
         `;
-    } else if (winner === 'computer') {
+    } else if(winner === 'computer') {
         //inc player score by 1
         scoreboard.computer++;
         //show modal result
         result.innerHTML = `
-        <h1 class="text-lose">Ouch...</h1>
+        <h1 class="text-lose">Ouch. You LOSE.</h1>
         <i class="fas fa-hand-${computerChoice} fa-10x"></i>
         <p>Computer chose <strong>${computerChoice}</strong></p>
         `;
     } else {
         result.innerHTML = `
-        <h1 class="text-lose">Twinsies!</h1>
+        <h1>Twinsies</h1>
         <i class="fas fa-hand-${computerChoice} fa-10x"></i>
         <p>Computer chose <strong>${computerChoice}</strong></p>
         `;
@@ -92,5 +91,27 @@ function showWinner (winner, computerChoice) {
     modal.style.display = 'block';
 };
 
+// restart game
+function restartGame() {
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `
+        <p>Player: 0</p>
+        <p>Computer: 0</p>
+    `;
+}
+
+// clear modal
+function clearModal(e) {
+    if(e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
 //event listeners
 choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
+
+
+
